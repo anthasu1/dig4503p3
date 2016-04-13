6ae16e43297b359c706defa5a0584a63aed61a89
Joseph <anthasu1@gmail.com>
2016-04-12 21:38:01 -0400

    Stuff

--- petfinder/js/process.js
+++ petfinder/js/process.js
@@ -474,9 +474,9 @@ function likedPet(){
 	closebtn.innerHTML = "x";
 	closebtn.setAttribute("onclick", "this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode);");
 	
-	newAccordionPetInfo.innerHTML = "<div class='col1'><img class = 'accordionimage' src='"+likedphoto+"'></div><div class='col2'><p>Animal: "+likedanimal+"</p><ul>Breed: "+likedbreed+"</ul><p>Age: "+likedage+"</p><p>Sex: "+likedsex+"</p><p>Size: "+likedsize+"</p><ul>More Info: "+likedoption+"</ul><p>Description: "+likeddescription+"</p></div>";
+	newAccordionPetInfo.innerHTML = "<div class='col1'><img class = 'accordionimage' src='"+likedphoto+"'><p>Organization: "+likedshelter+"</p><p>Email: "+likedemail+"</p><p>Phone: "+likedphone+"</p></div><div class='col2'><p>Animal: "+likedanimal+"</p><ul>Breed: "+likedbreed+"</ul><p>Age: "+likedage+"</p><p>Sex: "+likedsex+"</p><p>Size: "+likedsize+"</p><ul>More Info: "+likedoption+"</ul><p>Description: "+likeddescription+"</p></div>";
 	
-	newAccordionShelterInfo.innerHTML = "<div class='col3'><p>Organization: "+likedshelter+"</p><p>Email: "+likedemail+"</p><p>Phone: "+likedphone+"</p></div>";
+	/*newAccordionShelterInfo.innerHTML = "<p>Organization: "+likedshelter+"</p><p>Email: "+likedemail+"</p><p>Phone: "+likedphone+"</p>";*/
 	
 	newAccordionPetInfo.appendChild(closebtn);
 	newAccordionContent.appendChild(newAccordionPetInfo);
